af796d5c8c83529363a12c71883033a2a467a5de
Dylan-bak <jesuscarecrow@gmail.com>
2024-09-30 22:21:27 +0900

    Feat: [core] project initialize 에러 fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   // transpilePackages: ['@repo/ui'],
   compiler: { styledComponents: true },
   output: 'standalone',
